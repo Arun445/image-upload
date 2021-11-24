6171194a91f98990c578c20776d23fcaa9b1c79f
Arun445 <76538265+Arun445@users.noreply.github.com>
2021-11-24 17:29:43 +0200

    Delete uploadImageS3Login.ts

--- src/components/UploadImage/uploadImageS3Login.ts
+++
@@ -1,6 +0,0 @@
-export const s3Login = {
-  bucketName: "uploaded-images-map",
-  region: "eu-central-1",
-  accessKeyId: "AKIATUC6JYZG5PDFOKA6",
-  secretAccessKey: "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",
-};
