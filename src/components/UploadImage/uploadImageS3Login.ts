88ea721b81f7438e64e8c002d49aac6e78a1ad6e
aron <arunas.martinaitis1@gmai.com>
2021-11-24 17:30:28 +0200

    image

--- src/components/UploadImage/uploadImageS3Login.ts
+++ src/components/UploadImage/uploadImageS3Login.ts
@@ -2,5 +2,5 @@ export const s3Login = {
   bucketName: "uploaded-images-map",
   region: "eu-central-1",
   accessKeyId: "AKIATUC6JYZG5PDFOKA6",
-  secretAccessKey: "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",
+  secretAccessKey: "Cq0/evCOR8wScaKOtEMb6p3Zm5THHisFRngGMToasdO",
 };
